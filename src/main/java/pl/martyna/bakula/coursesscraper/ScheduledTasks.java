316c146de7a5035d669530ab245cd7eecc79cc8b
Martyna Bakula <martyna.bakula@currenda.pl>
2022-09-15 20:06:53 +0200

    Scheduled

---
+++ src/main/java/pl/martyna/bakula/coursesscraper/ScheduledTasks.java
@@ -0,0 +1,21 @@
+package pl.martyna.bakula.coursesscraper;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTasks {
+
+    private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(cron = "0 30 12,0 * * *")
+    public void reportCurrentTime() {
+        log.info("The time is now {}", dateFormat.format(new Date()));
+    }
+}
